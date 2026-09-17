df6a3a7758cc93a1bc4875dcbf58f1dd7abb59a5
thesalt0818 <thesalt0818@gmail.com>
2026-09-17 09:43:35 +0900

    팝업 사이즈 조절

--- index.30327.js
+++ index.30327.js
@@ -1,4 +1,4 @@
-System.register(["./application.35106.js"], function (_export, _context) {
+System.register(["./application.74bea.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
