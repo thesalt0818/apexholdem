a95d4d2c18803d31773303b6b936ac239263729e
thesalt0818 <thesalt0818@gmail.com>
2026-09-17 12:50:55 +0900

    기기별 ui 03

--- index.ddcf2.js
+++ index.ddcf2.js
@@ -1,4 +1,4 @@
-System.register(["./application.29f5d.js"], function (_export, _context) {
+System.register(["./application.11737.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
