d3976904130142361fa80c370623bb939893d80f
thesalt0818 <thesalt0818@gmail.com>
2026-09-17 12:36:50 +0900

    기기별 ui 02

--- index.4ca5c.js
+++ index.4ca5c.js
@@ -1,4 +1,4 @@
-System.register(["./application.c087c.js"], function (_export, _context) {
+System.register(["./application.29f5d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
