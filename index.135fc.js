e8cf9c40e33d51f9e91ccd4411ba002c63ed5780
thesalt0818 <thesalt0818@gmail.com>
2026-09-17 12:00:14 +0900

    기기별 ui

--- index.135fc.js
+++ index.135fc.js
@@ -1,4 +1,4 @@
-System.register(["./application.74bea.js"], function (_export, _context) {
+System.register(["./application.c087c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
