9618ef34d3a457b3b8b3ede9288bb5a83db09b7c
thesalt0818 <thesalt0818@gmail.com>
2026-09-17 13:34:06 +0900

    회전 ui 수정 01

--- index.37c53.js
+++ index.37c53.js
@@ -1,4 +1,4 @@
-System.register(["./application.11737.js"], function (_export, _context) {
+System.register(["./application.cefa6.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
