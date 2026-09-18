54a0e130a87bedb30b0544333183dd67ba40bee6
thesalt0818 <thesalt0818@gmail.com>
2026-09-18 15:12:10 +0900

    서버 배포 - render

--- index.2f9e6.js
+++ index.2f9e6.js
@@ -1,4 +1,4 @@
-System.register(["./application.ff856.js"], function (_export, _context) {
+System.register(["./application.c9078.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
